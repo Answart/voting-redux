adf2ce0217a477e815d7899e74eceb4afd4ad5f5
Alexandra Swart <answart@sbcglobal.net>
2018-03-27 21:43:30 -0700

    Add redux-form

--- client/src/core/reducers.js
+++ client/src/core/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
 
 
 export default combineReducers({
+  form,
   router: routerReducer
 });
