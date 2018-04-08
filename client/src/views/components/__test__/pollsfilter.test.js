4ae3ec14271619088dd94ab5dd47f13d5167a9fc
Alexandra Swart <answart@sbcglobal.net>
2018-04-07 22:18:39 -0700

    Add Pollsfilter component

---
+++ client/src/views/components/__test__/pollsfilter.test.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// Import components
+import PollsFilter from '../pollsfilter';
+
+const mockFn = jest.fn();
+const filters = [{
+  label: 'User',
+  key: 'user_name',
+  value: 'Alexandra'
+}];
+const pollColumnData = [
+  {id:0, key:'open', label:'Status', numeric:false, colSpan:10 },
+  {id:1, key:'votes', label:'Votes', numeric:false, colSpan:10 },
+  {id:2, key:'title', label:'Title', numeric:false, colSpan:40 },
+  {id:3, key:'user_name', label:'User', numeric:false, colSpan:20 },
+  {id:4, key:'date_created', label:'Date', numeric:false, colSpan:20 }
+];
+const props = {
+  pollColumnData,
+  filters,
+  loadFilteredPolls: mockFn,
+};
+pollColumnData.pop();
+const filterDropDownList = [ ...pollColumnData ];
+
+
+describe('<PollsFilter />', () => {
+  let wrapper, cmpnt, state,
+    keyField, valueField,
+    historyPushSpy, loadFilteredPollsSpy;
+
+  beforeAll(async () => {
+    loadFilteredPollsSpy = jest.spyOn(props, 'loadFilteredPolls');
+    wrapper = mountWithRouterConnected(<PollsFilter {...props} />);
+    wrapper.find('MemoryRouter').instance().history.push = mockFn;
+    historyPushSpy = jest.spyOn(wrapper.find('MemoryRouter').instance().history, 'push');
+    wrapper.update();
+    await wrapper.find(PollsFilter).instance().componentDidMount();
+    cmpnt = wrapper.find('PollsFilter');
+    await asyncFlush();
+    keyField = cmpnt.find('TextField#filter-key');
+    valueField = cmpnt.find('TextField#filter-value');
+  });
+  beforeEach(() => state = wrapper.find('PollsFilter').instance().state);
+  afterEach(() => jest.clearAllMocks());
+
+  it('renders properly', () => {
+    expect(cmpnt).toHaveLength(1);
+    expect(cmpnt.prop('pollColumnData')).toEqual(pollColumnData);
+    expect(cmpnt.prop('filters')).toEqual(filters);
+    expect(typeof cmpnt.prop('loadFilteredPolls')).toBe('function');
+    expect(typeof cmpnt.instance().handleFilterPolls).toBe('function');
+    expect(typeof cmpnt.instance().handleSetState).toBe('function');
+    expect(typeof cmpnt.instance().handleAddFilter).toBe('function');
+    expect(typeof cmpnt.instance().handleRemoveFilter).toBe('function');
+    expect(typeof cmpnt.instance().handleFilterChange).toBe('function');
+    expect(cmpnt.instance().state).toEqual({
+      filterDropDownList,
+      filters,
+      label: '',
+      key: '',
+      value: '',
+      keyInvalid: false,
+      valueInvalid: false,
+    });
+    expect(mountToJson(cmpnt)).toMatchSnapshot();
+  });
+
+  describe('Filter Key Field', () => {
+    it('renders properly', () => {
+      expect(keyField).toHaveLength(1);
+      expect(keyField.prop('label')).toBe('Filter by:');
+      expect(keyField.prop('error')).toBe(false);
+      expect(keyField.prop('disabled')).toBe(false);
+      expect(typeof keyField.prop('onChange')).toBe('function');
+      expect(keyField.prop('helperText')).toBe('');
+    });
+    it('changes state "label" and "key" on field change', () => {
+      expect(cmpnt.find('TextField#filter-value').prop('disabled')).toBe(true);
+      expect(state.key).toBe('');
+      fillInput(wrapper, 'filter-key', { target: { value: 'title' }, nativeEvent: { target: { innerText: 'Title' }}});
+      expect(wrapper.find('PollsFilter').instance().state.key).toBe('title');
+      expect(wrapper.find('PollsFilter').instance().state.label).toBe('Title');
+      expect(wrapper.find('TextField#filter-value').prop('disabled')).toBe(false);
+      expect(wrapper.find('Btn').prop('disabled')).toBe(true);
+    });
+  });
+
+  describe('Filter Value Field', () => {
+    it('renders properly', () => {
+      expect(valueField).toHaveLength(1);
+      expect(valueField.prop('label')).toBe('Value:');
+      expect(valueField.prop('error')).toBe(false);
+      expect(valueField.prop('disabled')).toBe(true);
+      expect(typeof valueField.prop('onChange')).toBe('function');
+      expect(valueField.prop('helperText')).toBe('');
+    });
+    it('changes state "value" on field change', () => {
+      expect(wrapper.find('Btn').prop('disabled')).toBe(true);
+      expect(state.value).toBe('');
+      fillInput(wrapper, 'filter-key', { target: { value: 'title' }, nativeEvent: { target: { innerText: 'Title' }}});
+      fillInput(wrapper, 'filter-value', { target: { value: 'Random Title' }});
+      const newState = wrapper.find('PollsFilter').instance().state;
+      expect(newState.key).toBe('title');
+      expect(newState.label).toBe('Title');
+      expect(newState.value).toBe('Random Title');
+      expect(wrapper.find('Btn').prop('disabled')).toBe(false);
+    });
+  });
+
+  it('"Add Filter" click calls loadFilteredPolls()', () => {
+    expect(loadFilteredPollsSpy).not.toHaveBeenCalled();
+    fillInput(wrapper, 'filter-key', { target: { value: 'title' }, nativeEvent: { target: { innerText: 'Title' }}});
+    fillInput(wrapper, 'filter-value', { target: { value: 'Random Title' }});
+    const button = wrapper.find('Btn');
+    expect(button.prop('disabled')).toBe(false);
+    expect(button.prop('text')).toBe('Add Filter');
+    expect(button.prop('size')).toBe('small');
+    expect(typeof button.prop('onClick')).toBe('function');
+    clickButton(button);
+    wrapper.update();
+    expect(loadFilteredPollsSpy).toHaveBeenCalled();
+    expect(loadFilteredPollsSpy).toHaveBeenCalledTimes(1);
+    const newFilters = [
+      { label: 'User', key: 'user_name', value: 'Alexandra' },
+      { label: 'Title', key: 'title', value: 'Random Title' }
+    ];
+    expect(loadFilteredPollsSpy).toHaveBeenCalledWith(newFilters);
+    expect(wrapper.find('PollsFilter').instance().state.filters).toEqual(newFilters);
+  });
+
+  describe('Chips', () => {
+    let exampleChip;
+    beforeAll(() => {
+      fillInput(wrapper, 'filter-key', { target: { value: 'title' }, nativeEvent: { target: { innerText: 'Title' }}});
+      fillInput(wrapper, 'filter-value', { target: { value: 'Random Title' }});
+      clickButton(wrapper.find('Btn'));
+      wrapper.update();
+      exampleChip = wrapper.find('Chip#chip-user_name');
+    });
+
+    it('renders properly', () => {
+      expect(state.filters).toEqual([
+        { label: 'User', key: 'user_name', value: 'Alexandra' },
+        { label: 'Title', key: 'title', value: 'Random Title' }
+      ]);
+      expect(wrapper.find('Chip')).toHaveLength(2);
+      expect(exampleChip.prop('label')).toEqual(`User: Alexandra`);
+      expect(typeof exampleChip.prop('onDelete')).toEqual('function');
+    });
+    it('resets entire state with updated "filters" on click', async () => {
+      expect(loadFilteredPollsSpy).not.toHaveBeenCalled();
+      exampleChip.prop('onDelete')();
+      expect(loadFilteredPollsSpy).toHaveBeenCalled();
+      expect(loadFilteredPollsSpy).toHaveBeenCalledTimes(1);
+      expect(loadFilteredPollsSpy).toHaveBeenCalledWith([{ label: 'Title', key: 'title', value: 'Random Title' }]);
+      const state2 = wrapper.find('PollsFilter').instance().state;
+      expect(state2).toEqual({
+        filterDropDownList,
+        filters: [{ label: 'Title', key: 'title', value: 'Random Title' }],
+        label: '',
+        key: '',
+        value: '',
+        keyInvalid: false,
+        valueInvalid: false
+      }
+    );
+    });
+  });
+});
---
+++ client/src/views/components/pollsfilter.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+// Import material-ui
+import Grid from 'material-ui/Grid';
+import Chip from 'material-ui/Chip';
+import TextField from 'material-ui/TextField';
+import { MenuItem } from 'material-ui/Menu';
+// Import components
+import Btn from './btn';
+
+
+class PollsFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterDropDownList: null,
+      filters: null,
+      label: '',
+      key: '',
+      value: '',
+      keyInvalid: false,
+      valueInvalid: false,
+    }
+    this.handleFilterPolls = this.handleFilterPolls.bind(this);
+    this.handleSetState = this.handleSetState.bind(this);
+    this.handleAddFilter = this.handleAddFilter.bind(this);
+    this.handleRemoveFilter = this.handleRemoveFilter.bind(this);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+  }
+  componentWillMount = () => {
+    const { filters, pollColumnData } = this.props;
+    const filterDropDownList = pollColumnData.filter(item => item.key !== 'date_created');
+    this.setState({ filters, filterDropDownList });
+    // this.handleSetState(filters);
+    // this.handleFilterPolls();
+  }
+
+  handleFilterPolls = () => {
+    // const { loadFilteredPolls } = this.props;
+    // const { filters } = this.state;
+    this.props.loadFilteredPolls(this.state.filters);
+  };
+
+  handleSetState = (filters = null, newFilter = null) => {
+    if (!!newFilter) {
+      (!!filters && filters.length > 0) ? filters.push(newFilter) : (filters = [newFilter]);
+    }
+    this.setState({
+      filters,
+      label: '',
+      key: '',
+      value: '',
+      keyInvalid: false,
+      valueInvalid: false
+    }, () => this.handleFilterPolls());
+  }
+
+  handleFilterChange = name => event => {
+    if (name === 'key') {
+      const key = event.target.value;
+      const label = event.nativeEvent.target.innerText;
+      this.setState({ key, label });
+    } else {
+      this.setState({ [name]: event.target.value });
+    }
+  };
+  handleAddFilter = () => {
+    const { label, key, value, filters } = this.state;
+    if (!!filters && filters.find(filter => filter.key === key)) {
+      this.setState({ keyInvalid: true })
+    } else if (!value.length) {
+      this.setState({ valueInvalid: true })
+    } else {
+      let newVal = value.trim();
+      if (key === 'votes') {
+        newVal = Number(newVal);
+      } else if (key === 'open') {
+        newVal = Boolean(newVal.toLowerCase() === 'open');
+      }
+      this.handleSetState(filters, { label, key, value: newVal });
+    }
+  };
+  handleRemoveFilter = filter => event => {
+    const { filters } = this.state;
+    const updatedFilters = [...filters];
+    const toDelete = updatedFilters.indexOf(filter);
+    updatedFilters.splice(toDelete, 1);
+    this.handleSetState(updatedFilters);
+  };
+
+  render() {
+    const {
+      key, value,
+      keyInvalid, valueInvalid,
+      filters,
+      filterDropDownList
+    } = this.state;
+    return (
+      <Grid container
+        direction='row'
+        justify='center'
+        alignItems='flex-start'
+      >
+        {/* Filter Form Area */}
+        <Grid item xs={12} sm={3}>
+          <TextField
+            id='filter-key'
+            label='Filter by:'
+            value={key}
+            error={keyInvalid}
+            disabled={!!value && value.length > 0}
+            onChange={this.handleFilterChange('key')}
+            helperText={keyInvalid ? '* Filter already made' : ''}
+            InputLabelProps={{ shrink: true }}
+            style={{ display:'flex', margin:5, paddingTop:3 }}
+            margin='normal'
+            select
+          >
+            <MenuItem value=''> -- </MenuItem>
+            {filterDropDownList.map(item => {
+              return (
+                <MenuItem key={item.id} value={item.key}>
+                  {item.label}
+                </MenuItem>
+              )
+            })}
+          </TextField>
+        </Grid>
+        <Grid item xs={12} sm={7}>
+          <TextField
+            id='filter-value'
+            label='Value:'
+            type={(!!key && key === 'votes') ? 'number' : 'text'}
+            value={value}
+            error={valueInvalid}
+            disabled={!key}
+            helperText={(!!key && key === 'open') ? `Filter for 'open' or 'closed' polls` : ''}
+            onChange={this.handleFilterChange('value')}
+            InputLabelProps={{ shrink: true }}
+            style={{ display:'flex', margin:5 }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}
+          style={{ textAlign:'center' }}
+        >
+          <Btn
+            size='small'
+            text='Add Filter'
+            disabled={!key || !value}
+            onClick={this.handleAddFilter.bind()}
+            style={{ marginBottom:10 }}
+          />
+        </Grid>
+        {/* Filter Chips Area */}
+        {!!filters && filters.length > 0 && (
+          <Grid item xs={12} sm={10}
+            style={{ display:'flex', justifyContent:'center', flexWrap:'wrap', padding:5 }}
+          >
+            {filters.map(filter => {
+              return (
+                <Chip
+                  id={`chip-${filter.key}`}
+                  key={filter.key}
+                  avatar={null}
+                  label={`${filter.label}: ${filter.value}`}
+                  onDelete={this.handleRemoveFilter(filter)}
+                  style={{ margin:'1px 6px 6px 6px' }}
+                />
+              )
+            })}
+          </Grid>
+        )}
+      </Grid>
+    )
+  }
+};
+
+PollsFilter.propTypes = {
+  pollColumnData: PropTypes.array.isRequired,
+  filters: PropTypes.array,
+  loadFilteredPolls: PropTypes.func.isRequired
+}
+
+export default connect(null, null)(PollsFilter);
