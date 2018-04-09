03e290dc9c80fce386ff660b055282af8442deb2
Alexandra Swart <answart@sbcglobal.net>
2018-04-09 14:29:26 -0700

    Wrap users actions tests in "userActions" describe

--- client/src/core/users/__test__/userActions.test.js
+++ client/src/core/users/__test__/userActions.test.js
@@ -18,37 +18,39 @@ const values = {
 }
 
 
-describe('authUser()', () => {
-  it('"register" returns an object with the type of AUTH_USER', () => {
-    expect(userActions.authUser('register', { ...values }, { ...promises })).toEqual({
-      type: AUTH_USER,
-      authType: 'register',
-      ...values,
-      ...promises
+describe('userActions', () => {
+  describe('authUser()', () => {
+    it('"register" returns an object with the type of AUTH_USER', () => {
+      expect(userActions.authUser('register', { ...values }, { ...promises })).toEqual({
+        type: AUTH_USER,
+        authType: 'register', 
+        ...values,
+        ...promises
+      });
+    });
+    it('"login" returns an object with the type of AUTH_USER', () => {
+      expect(userActions.authUser('login', { ...values }, { ...promises })).toEqual({
+        type: AUTH_USER,
+        authType: 'login',
+        ...values,
+        ...promises
+      });
     });
   });
-  it('"login" returns an object with the type of AUTH_USER', () => {
-    expect(userActions.authUser('login', { ...values }, { ...promises })).toEqual({
-      type: AUTH_USER,
-      authType: 'login',
-      ...values,
-      ...promises
-    });
-  });
-});
 
-describe('logoutUser()', () => {
-  it('returns an object with the type of RESET_AUTHED_USER', () => {
-    expect(userActions.logoutUser()).toEqual({
-      type: RESET_AUTHED_USER
+  describe('logoutUser()', () => {
+    it('returns an object with the type of RESET_AUTHED_USER', () => {
+      expect(userActions.logoutUser()).toEqual({
+        type: RESET_AUTHED_USER
+      });
     });
   });
-});
 
-describe('deleteUser()', () => {
-  it('returns an object with the type of DELETE_USER', () => {
-    expect(userActions.deleteUser()).toEqual({
-      type: DELETE_USER
+  describe('deleteUser()', () => {
+    it('returns an object with the type of DELETE_USER', () => {
+      expect(userActions.deleteUser()).toEqual({
+        type: DELETE_USER
+      });
     });
   });
 });
