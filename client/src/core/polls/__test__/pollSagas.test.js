df59d730cc7a544636609df18dbcf4ce3e9759d5
Alexandra Swart <answart@sbcglobal.net>
2018-04-12 00:14:41 -0700

    Create postPoll sagas

--- client/src/core/polls/__test__/pollSagas.test.js
+++ client/src/core/polls/__test__/pollSagas.test.js
@@ -1,15 +1,86 @@
 import { call, put, fork, select, takeLatest } from 'redux-saga/effects';
 import { SubmissionError, reset } from 'redux-form';
 import { cloneableGenerator } from 'redux-saga/utils';
+import fetchMock from 'fetch-mock';
 // Import compoenents
 import history from '../../history';
-import { pollActions, pollReducer, pollSagas } from '../../polls';
+import { pollActions, pollReducer, postPollApi } from '../../polls';
+import { getAuthedUser } from '../../users';
+import {
+  watchPostPoll, watchPostPollSuccess,
+  postPoll, postPollSuccess
+} from '../../polls/sagas';
+import {
+  POST_POLL, POST_POLL_SUCCESS, POST_POLL_FAILURE
+} from '../../constants';
+import { requestOpts, requestApi } from '../../helpers';
+
 
 
 describe('pollSagas', () => {
 
-  describe('watchers', () => {});
+  describe('watchers', () => {
 
-  describe('watched sagas', () => {});
+    it('watchPostPoll() calls takeLatest on POST_POLL action', () => {
+      const gen = watchPostPoll();
+      expect(gen.next().value).toEqual(takeLatest(POST_POLL, postPoll));
+      expect(gen.next()).toEqual({ done: true, value: undefined });
+    });
+    it('watchPostPollSuccess() calls takeLatest on POST_POLL_SUCCESS action', () => {
+      const gen = watchPostPollSuccess();
+      expect(gen.next().value).toEqual(takeLatest(POST_POLL_SUCCESS, postPollSuccess));
+      expect(gen.next()).toEqual({ done: true, value: undefined });
+    });
+  });
+
+  describe('watched sagas', () => {
+    let poll, promises;
+    beforeAll(() => {
+      let resolveIt, rejectIt;
+      var grabPromises = new Promise(function(resolve, reject) {
+        resolveIt = resolve;
+        rejectIt = reject;
+      });
+      promises = { resolve: resolveIt, reject: rejectIt };
+      poll = {
+        title: 'Best Spaniel Breed',
+        choices: 'Cocker Spaniel, Springer Spaniel'
+       };
+    });
+
+    describe('post poll flow', () => {
+      let postPollAction, clone;
+      beforeAll(() => postPollAction = pollActions.postPoll(poll, promises));
+      beforeEach(() => {
+        const gen = cloneableGenerator(postPoll)(postPollAction);
+        clone = gen.clone();
+      });
+
+      it('completes successfully on success', () => {
+        const postPollApi = fetch;
+        expect(clone.next().value).toEqual(select(getAuthedUser));
+        expect(clone.next().value).toEqual(call(postPollApi, poll.title, poll.choices, 'public', 'public'));
+        expect(clone.next().value).toEqual(put({
+          type: POST_POLL_SUCCESS,
+          poll,
+          message: 'success'
+        }));
+        expect(clone.next().value).toEqual(put(reset('newPoll')));
+        expect(clone.next().value).toEqual(call(promises.resolve));
+        expect(clone.next().done).toEqual(true);
+      });
+      it('throws successfully on failure', () => {
+        clone.next();
+        const error = 'Error creating poll.';
+        expect(clone.throw(error).value)
+          .toEqual(put({ type: POST_POLL_FAILURE, error }));
+        expect(clone.next().value)
+          .toEqual(call(promises.reject, (new SubmissionError(error))));
+        expect(clone.next().done).toEqual(true);
+      });
+    });
+
+
+  });
 
 });
--- client/src/core/polls/sagas.js
+++ client/src/core/polls/sagas.js
@@ -2,15 +2,52 @@ import { call, put, fork, select, takeLatest } from 'redux-saga/effects';
 import { SubmissionError, reset } from 'redux-form';
 // Import compoenents
 import history from '../history';
+import { getAuthedUser } from '../users';
+import {
+  getViewedId
+} from '../polls';
+import {
+  POST_POLL, POST_POLL_SUCCESS, POST_POLL_FAILURE
+} from '../constants';
+
+
+export function* postPoll(action) {
+  const { title, choices, resolve, reject } = action;
+  try {
+    let authedUser = yield select(getAuthedUser);
+    const user = (!!authedUser ? authedUser : { name: 'public', cuid: 'public' });
+    const response { message: 'yayay', poll: { cuid: '12314', title: 'blah', choices: [] }};
+    yield put({ type: POST_POLL_SUCCESS, poll: response.poll, message: response.message });
+    yield put(reset('newPoll'));
+    yield call(resolve);
+  } catch (error) {
+    yield put({ type: POST_POLL_FAILURE, error });
+    yield call(reject, (new SubmissionError(error)));
+  }
+};
+export function* postPollSuccess() {
+  const viewedId = yield select(getViewedId);
+  yield history.push(`/poll/${viewedId}`);
+};
 
 
 //=====================================
 //  WATCHERS
 //-------------------------------------
 
+export function* watchPostPoll() {
+  yield takeLatest(POST_POLL, postPoll);
+};
+export function* watchPostPollSuccess() {
+  yield takeLatest(POST_POLL_SUCCESS, postPollSuccess);
+};
+
 
 //=====================================
 //  ROOT
 //-------------------------------------
 
-export const pollSagas = [];
+export const pollSagas = [
+  fork(watchPostPoll),
+  fork(watchPostPollSuccess),
+];
