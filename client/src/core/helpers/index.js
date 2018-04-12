2d3e473561b5a99db781d97cb783cd22035412c5
Alexandra Swart <answart@sbcglobal.net>
2018-04-11 22:42:51 -0700

    Add reducer helper functions to helpers folder

--- client/src/core/helpers/index.js
+++ client/src/core/helpers/index.js
@@ -1,1 +1,2 @@
 export { requestOpts, requestApi } from './api.helper';
+export { getUpdatedList, getFilteredList, getItemById } from './reducer.helper';
---
+++ client/src/core/helpers/reducer.helper.js
@@ -0,0 +1,43 @@
+
+
+export function getUpdatedList(list, item, filters = null) {
+  if (!list) return null;
+  if (!item) return list;
+
+  const newItem = list.every(existingItem => item.cuid !== existingItem.cuid);
+  if (!!filters && filters.length > 0) {
+    const itemPassesFilter = filters.every(filter => item[`${filter.key}`] === filter.value);
+    if (!itemPassesFilter) return list;
+
+    if (newItem) {
+      list.push(item);
+      return list;
+    } else {
+      list.map(existingItem => existingItem.cuid === item.cuid ? item : existingItem);
+      return list;
+    }
+  } else {
+    if (newItem) {
+      list.push(item);
+      return list;
+    } else {
+      list.map(existingItem => existingItem.cuid === item.cuid ? item : existingItem);
+      return list;
+    }
+  }
+};
+
+export function getFilteredList(list = null, filters = null) {
+  if (!list || (!!list && list.length === 0)) return null;
+  if (!filters || filters.length <= 0) return list;
+
+  const filteredList = list.filter(item => filters.every(filter => item[`${filter.key}`] === filter.value));
+  return filteredList;
+};
+
+export function getItemById(list, id = null) {
+  if (!id || (!list || (!!list && list.length === 0))) return null
+
+  const itemsById = list.filter(item => item.cuid === id);
+  return !!itemsById[0] ? itemsById[0] : null;
+};
