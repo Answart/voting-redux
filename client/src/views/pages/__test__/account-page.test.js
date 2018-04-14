b9d9e17a8531b93f4baea6051488aeea18ab0cf2
Alexandra Swart <answart@sbcglobal.net>
2018-04-14 00:12:42 -0700

    Update AccountPage with authed state state tests

--- client/src/views/pages/__test__/account-page.test.js
+++ client/src/views/pages/__test__/account-page.test.js
@@ -11,21 +11,23 @@ const props = {
 
 
 describe('<AccountPage />', () => {
-  let wrapper, openVotePollPopupSpy, fetchPollsSpy;
-  beforeAll(() => {
-    wrapper = mountWithRouterConnected(<AccountPage {...props} />)
-    wrapper.update();
+  let wrapper, openNewPollPopupSpy, deleteUserSpy, goToUserPollsSpy;
+  beforeAll(async () => {
+    openNewPollPopupSpy = jest.spyOn(props, 'openNewPollPopup');
+    deleteUserSpy = jest.spyOn(props, 'deleteUser');
+    goToUserPollsSpy = jest.spyOn(props, 'goToUserPolls');
+    wrapper = mountWithRouterConnected(<AccountPage {...props} />);
+    await wrapper.find(AccountPage).instance().componentDidMount();
   });
 
   it('renders properly', () => {
     const page = wrapper.find('AccountPage');
-    expect(mountToJson(page)).toMatchSnapshot();
     expect(page).toHaveLength(1);
     expect(typeof page.prop('authedUserState')).toBe('object');
     expect(typeof page.prop('openNewPollPopup')).toBe('function');
     expect(typeof page.prop('deleteUser')).toBe('function');
     expect(typeof page.prop('goToUserPolls')).toBe('function');
-    expect(page.find('Section').length).toBe(2);
+    expect(mountToJson(page)).toMatchSnapshot();
   });
 
   it('title renders properly', () => {
@@ -35,19 +37,34 @@ describe('<AccountPage />', () => {
     expect(title.text()).toEqual('Account');
   });
 
-  describe('authed', () => {
+  describe('UNauthed', () => {
     it('renders properly', () => {
-      expect(wrapper.find('Grid#account')).toHaveLength(1);
-      expect(Object.keys(wrapper.find(AccountPage).props()).length).toBe(3);
-      expect(wrapper.find('Section').length).toBe(2);
-      expect(wrapper.find('ActivityList').length).toBe(1);
+      expect(wrapper.find('Grid#account')).toHaveLength(0);
+      expect(wrapper.find('Section').length).toBe(0);
+      expect(wrapper.find('ActivityList').length).toBe(0);
     });
   });
 
-  // describe('UNauthed', () => {
-  //   // need to overwrite authUser prop in 'connect' (null user in state.users.authUser)
-  //   it('renders properly', () => {
-  //     expect(wrapper.find('Grid#account')).toHaveLength(0);
-  //   });
-  // });
+  describe('authed', () => {
+    let authedWrapper, openNewPollPopupSpy2, deleteUserSpy2, goToUserPollsSpy2, historyPushSpy2;
+    beforeAll(async () => {
+      openNewPollPopupSpy2 = jest.spyOn(props, 'openNewPollPopup');
+      deleteUserSpy2 = jest.spyOn(props, 'deleteUser');
+      goToUserPollsSpy2 = jest.spyOn(props, 'goToUserPolls');
+      authedWrapper = mountWithRouterConnected(<AccountPage {...props} />, ['/'], false);
+      authedWrapper.find('MemoryRouter').instance().history.push = mockFn;
+      historyPushSpy2 = jest.spyOn(authedWrapper.find('MemoryRouter').instance().history, 'push');
+      authedWrapper.update();
+      await authedWrapper.find(AccountPage).instance().componentDidMount();
+    });
+    afterAll(() => authedWrapper.unmount());
+
+    it('renders properly', () => {
+      expect(authedWrapper.find('Grid#account')).toHaveLength(1);
+      expect(Object.keys(authedWrapper.find(AccountPage).props()).length).toBe(3);
+      expect(authedWrapper.find('Section').length).toBe(2);
+      expect(authedWrapper.find('ActivityList').length).toBe(1);
+    });
+
+  });
 });
