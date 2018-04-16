5f5696fa662ec266df8100680a3f332c303ad0bd
Alexandra Swart <answart@sbcglobal.net>
2018-04-15 19:52:27 -0700

    PollsFilter is not case-sensitive

--- client/src/core/helpers/reducer.helper.js
+++ client/src/core/helpers/reducer.helper.js
@@ -1,12 +1,20 @@
 
 
+function isOrIsLike(originalVal, val) {
+  originalVal = originalVal.toLowerCase();
+  val = val.toLowerCase();
+  return ((originalVal === val) || (originalVal.includes(val)));
+}
+
+
 export function getUpdatedList(list, item, filters = null) {
   if (!list) return null;
   if (!item) return list;
 
   const newItem = list.every(existingItem => item.cuid !== existingItem.cuid);
   if (!!filters && filters.length > 0) {
-    const itemPassesFilter = filters.every(filter => item[`${filter.key}`] === filter.value);
+    const itemPassesFilter = filters.every(filter => isOrIsLike(item[`${filter.key}`], filter.value));
+
     if (!itemPassesFilter) return list;
 
     if (newItem) {
@@ -31,7 +39,7 @@ export function getFilteredList(list = null, filters = null) {
   if (!list || (!!list && list.length === 0)) return null;
   if (!filters || filters.length <= 0) return list;
 
-  const filteredList = list.filter(item => filters.every(filter => item[`${filter.key}`] === filter.value));
+  const filteredList = list.filter(item => filters.every(filter => isOrIsLike(item[`${filter.key}`], filter.value)));
   return filteredList;
 };
 
