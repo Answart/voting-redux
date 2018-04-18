84ac993fef2ea96a4e6c8f4b57bee0021677cef0
Alexandra Swart <answart@sbcglobal.net>
2018-04-18 14:34:27 -0700

    Move database config into own file from models folder

--- server/models/index.js
+++ server/models/index.js
@@ -1,32 +1,6 @@
-const mongoose = require('mongoose');
 
 
-module.exports.connect = (uri) => {
-
-  mongoose.connect(uri, (error) => {
-    if (error) {
-      console.error('Please make sure Mongodb is installed and running!');
-      throw error;
-    }
-  });
-
-  mongoose.Promise = global.Promise;
-
-  mongoose.connection.once('open', () => console.log('Connected to MongoDB!'));
-
-  mongoose.connection.on('error', err => {
-    console.error(`Mongoose connection error: ${err}`);
-    process.exit(1);
-  });
-
-  process.on('SIGINT', function() {
-    mongoose.connection.close(function() {
-      console.log('Mongoose default connection disconnected through app termination');
-      process.exit(0);
-    });
-  });
-
-  // Load models
+module.exports = () => {
   require('./user');
   require('./poll');
 }
--- server/server.js
+++ server/server.js
@@ -5,20 +5,22 @@ const passport = require('passport');
 const configurePassport = require('./config/passport');
 const configureExpress = require('./config/express');
 const configureRoutes = require('./config/routes');
-const models = require('./models');
-
-//=====================================
-//  INITIALIZE
-//-------------------------------------
+const configureDb = require('./config/database');
 
 const ENV_PRODUCTION = process.env.NODE_ENV === 'production';
 const app = express();
 
 
+//=====================================
+//  INITIALIZE
+
 configurePassport(app, passport);
+
 configureExpress(app, ENV_PRODUCTION);
+
 configureRoutes(app, passport);
-models.connect(process.env.MONGO_URL);
+
+configureDb(process.env.MONGO_URL);
 
 
 module.exports = app;
