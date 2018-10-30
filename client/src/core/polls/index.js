2e6e12a323a2bbd0994e564c560caf9a6d5475b4
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2018-10-29 20:32:48 -0700

    Update react-scripts to 2.0.5

--- client/src/core/polls/index.js
+++ client/src/core/polls/index.js
@@ -1,5 +1,5 @@
 export { pollActions, pollRequestActions } from './actions';
 export { pollSagas, pollSagaWatchers } from './sagas';
 export { pollReducer } from './reducer';
-export { getAllPolls, getPolls, getPollslist, getViewedPoll, getActivePoll, getViewedId } from './selectors';
+export { getAllPolls, getPolls, getViewedPoll, getActivePoll, getViewedId } from './selectors';
 export { getPollsApi, postPollApi, updatePollApi, updatePollVoteApi, deletePollApi } from './api';
