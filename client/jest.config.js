31da7f429f279fc5af957684f91f808340859710
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-17 00:31:43 -0800

    Update client packages

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,7 +5,9 @@ module.exports = {
   snapshotSerializers: ["enzyme-to-json/serializer"],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
+    "\\.js$": "<rootDir>/node_modules/babel-jest",
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       './src/utils/__test__/assetsTransformer.js',
   },
+  "transformIgnorePatterns": ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"]
 };
